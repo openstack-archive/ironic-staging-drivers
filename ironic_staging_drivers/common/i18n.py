5ad7c7c92597175751df770f1f30dec5ab69c67d
Lucas Alvares Gomes <lucasagomes@gmail.com>
2016-01-28 14:10:23 +0000

    Add Wake-On-Lan driver
    This patch is importing the Wake-On-Lan (WOL) driver and its documentation
    from ironic to ironic-staging-driver.
    
    Since we can't have duplicated entry points in setuptools we had to
    rename the driver names as following:
    
    pxe_wol   -> pxe_wol_iscsi
    agent_wol -> pxe_wol_agent
    fake_wol  -> fake_wol_fake
    
    This patch is using the "<boot>_<power>_<deploy>" template to name the
    drivers consistently.
    
    Change-Id: I2b051494fdba7bf6ca30d8f7bb406511bf7d4d76

--- ironic_staging_drivers/common/i18n.py
+++ ironic_staging_drivers/common/i18n.py
@@ -1,3 +1,6 @@
+# Copyright 2016 Red Hat, Inc.
+# All Rights Reserved.
+#
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
 #    a copy of the License at
@@ -10,10 +13,19 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from ironic_staging_drivers.tests import base
+import oslo_i18n as i18n
 
+_translators = i18n.TranslatorFactory(domain='ironic-staging-drivers')
 
-class FooTestCase(base.TestCase):
+# The primary translation function using the well-known name "_"
+_ = _translators.primary
 
-    def test_foo(self):
-        pass
+# Translators for log levels.
+#
+# The abbreviated names are meant to reflect the usual use of a short
+# name like '_'. The "L" is for "log" and the other letter comes from
+# the level.
+_LI = _translators.log_info
+_LW = _translators.log_warning
+_LE = _translators.log_error
+_LC = _translators.log_critical
