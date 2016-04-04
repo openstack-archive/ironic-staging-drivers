af3a1c12b98f137bca76542098f5947893a6c973
karimb <karimboumedhel@gmail.com>
2016-04-04 18:35:53 +0200

    Ovirt support
    This patch introduces new type of power and management drivers for ovirt vms.
    
    Change-Id: Iebf31db70dfd800d47399c69d7b2255467a87a1d
    Closes-Bug: #1564841

---
+++ ironic_staging_drivers/ovirt/__init__.py
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+"""
+PXE Driver and supporting meta-classes.
+"""
+
+from ironic.drivers import generic
+
+from ironic_staging_drivers.ovirt import ovirt
+
+
+class oVirtHardware(generic.GenericHardware):
+    """oVirt hardware type.
+
+    Uses oVirt for power and management.
+    """
+
+    @property
+    def supported_management_interfaces(self):
+        """List of supported management interfaces."""
+        return [ovirt.oVirtManagement]
+
+    @property
+    def supported_power_interfaces(self):
+        """List of supported power interfaces."""
+        return [ovirt.oVirtPower]
