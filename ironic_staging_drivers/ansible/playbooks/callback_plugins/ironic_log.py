d308f5f7e5377e01b5da41078c14ae10312e2953
Pavlo Shchelokovskyy <pshchelokovskyy@mirantis.com>
2016-06-02 20:43:54 +0300

    Add Ansible-based deploy driver
    This is Ansible deploy driver proof of concept.
    
    Ansible is invoked as a CLI command via Python's `subprocess` library.
    
    Each action is described by single playbook with roles, which is run whole.
    Control of deployment types and cleaning steps is through tags and
    auxiliary steps file for cleaning.
    The playbooks for actions can be set per-node, as is cleaning steps
    file.
    
    Supports whole images and partition images with local boot:
    - compressed images are downloaded to RAM and converted to disk device;
    - raw images are streamed to disk directly from HTTP;
    - config drive partition is currently supported for partition images only
    
    Root device hints are currently not supported (first device returned as
    `ansible_devices` fact is used), but support them is planned.
    
    Cleaning is supported, only one clean step for disk cleaning in PoC.
    Includes an example "light-weight" cleaning playbook that only wipes
    the partition tables.
    As in the case of this driver all cleaning steps are known to conductor,
    booting the bootstrap image is completely skipped when
    there are no cleaning steps to perform.
    
    This implementation does not rely on any local temporary files
    (except for config drive file).
    The Ansible inventory is built in-memory,
    and all the input variables are passed in as CLI arguments.
    
    Supports two modes for continuing deployment:
    - polling the node until the ssh port is open as part of a playbook;
    - having the bootstrap image calling back to Ironic API (default)
    
    Includes and makes use of two custom Ansible modules:
    - streaming download from http(s) source to the disk device directly;
    - creating partitions with `parted` utility.
    
    Logging is implemented as custom Ansible callback module,
    that used oslo.log and oslo.config to interleave Ansbile event log
    into the log file configured in `ironic.conf` (has some quirks in DevStack
    due to default logging system there).
    
    Neutron is mandatory.
    Tested with and targets Ansible 2.x, although should work with Ansible
    1.9.x as well (example playbooks for this case are provided
    and had some minimal testing).
    
    Current deploy ramdisk requirements:
    - password-less sudo permissions for the user used by Ansible
    - python 2.7.x
    - openssh-server
    - GNU coreutils
    - utils-linux
    - parted
    - qemu-utils
    - python-requests (for Ironic callback and streaming image download)
    - python-netifaces (for Ironic callback)
    
    Experimental DIB element: https://review.openstack.org/#/c/239447/
    Experimental TinyCore-based bootstrap:
    Ie39ce67dc93e7d53bf75937c7defacafad5fbfcf
    
    Tested on DevStack with VMs and real IPMI hardware.
    Tempest's baremetal_server_basic_ops test reliably passes with
    pxe_ssh_ansible driver.
    
    Change-Id: Ib9317d365d7bc39aa00a9d9e1eadddd2f9b2947f
    Related-bug: #1526308
    Co-Authored-By: Yuriy Zveryanskyy <yzveryanskyy@mirantis.com>

---
+++ ironic_staging_drivers/ansible/playbooks/callback_plugins/ironic_log.py
@@ -0,0 +1,129 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import ConfigParser
+import os
+
+from oslo_config import cfg
+from oslo_log import log as logging
+
+from ironic.common import i18n
+from ironic import version
+
+
+basename = os.path.splitext(__file__)[0]
+config = ConfigParser.ConfigParser()
+ironic_config = None
+ironic_log_file = None
+try:
+    config.readfp(open(basename + ".ini"))
+    if config.has_option('ironic', 'config_file'):
+        ironic_config = config.get('ironic', 'config_file')
+    if config.has_option('ironic', 'log_file'):
+        ironic_log_file = config.get('ironic', 'log_file')
+except Exception:
+    pass
+
+CONF = cfg.CONF
+DOMAIN = 'ironic'
+LOG = logging.getLogger(__name__, project=DOMAIN,
+                        version=version.version_info.release_string())
+logging.register_options(CONF)
+
+conf_kwargs = dict(args=[], project=DOMAIN,
+                   version=version.version_info.release_string())
+if ironic_config:
+    conf_kwargs['default_config_files'] = [ironic_config]
+CONF(**conf_kwargs)
+
+if ironic_log_file:
+    CONF.set_override("log_file", ironic_log_file)
+CONF.set_override("use_stderr", False)
+
+logging.setup(CONF, DOMAIN)
+
+
+class CallbackModule(object):
+
+    CALLBACK_VERSION = 2.0
+    CALLBACK_TYPE = 'notification'
+    CALLBACK_NAME = 'ironic_log'
+    CALLBACK_NEEDS_WHITELIST = True
+
+    def __init__(self, display=None):
+        self.node = None
+
+    def runner_msg_dict(self, result):
+        self.node = result._host.get_name()
+        name = result._task.get_name()
+        res = str(result._result)
+        return dict(node=self.node, name=name, res=res)
+
+    def v2_playbook_on_task_start(self, task, is_conditional):
+        # NOTE(pas-ha) I do not know (yet) how to obtain a ref to host
+        # until first task is processed
+        node = self.node or "Node"
+        name = task.get_name()
+        if name == 'setup':
+            LOG.info(i18n._LI(
+                "Processing task %(name)s."), dict(name=name, node=node))
+        else:
+            LOG.info(i18n._LI(
+                "Processing task %(name)s on node %(node)s."),
+                dict(name=name, node=node))
+
+    def v2_runner_on_failed(self, result, ignore_errors=False):
+        if ignore_errors:
+            LOG.warning(i18n._LW(
+                "Ansible task %(name)s failed on node %(node)s: %(res)s"),
+                self.runner_msg_dict(result))
+        else:
+            LOG.error(i18n._LE(
+                "Ansible task %(name)s failed on node %(node)s: %(res)s"),
+                self.runner_msg_dict(result))
+
+    def v2_runner_on_ok(self, result):
+        msg_dict = self.runner_msg_dict(result)
+        if msg_dict['name'] == 'setup':
+            LOG.info(i18n._LI(
+                "Ansible task 'setup' complete on node %(node)s"),
+                msg_dict)
+        else:
+            LOG.info(i18n._LI(
+                "Ansible task %(name)s complete on node %(node)s: %(res)s"),
+                msg_dict)
+
+    def v2_runner_on_unreachable(self, result):
+        LOG.error(i18n._LE(
+            "Node %(node)s was unreachable for Ansible task %(name)s: "
+            "%(res)s"),
+            self.runner_msg_dict(result))
+
+    def v2_runner_on_async_poll(self, result):
+        LOG.debug("Polled ansible task %(name)s for complete "
+                  "on node %(node)s: %(res)s",
+                  self.runner_msg_dict(result))
+
+    def v2_runner_on_async_ok(self, result):
+        LOG.info(i18n._LI(
+            "Async Ansible task %(name)s complete on node %(node)s: %(res)s"),
+            self.runner_msg_dict(result))
+
+    def v2_runner_on_async_failed(self, result):
+        LOG.error(i18n._LE(
+            "Async Ansible task %(name)s failed on node %(node)s: %(res)s"),
+            self.runner_msg_dict(result))
+
+    def v2_runner_on_skipped(self, result):
+        LOG.debug("Ansible task %(name)s skipped on node %(node)s: %(res)s",
+                  self.runner_msg_dict(result))
