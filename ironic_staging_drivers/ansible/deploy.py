f5a983a0f3f883f3b8ef71964d9ae7f784ff5d9d
Yuriy Zveryanskyy <yzveryanskyy@mirantis.com>
2016-06-09 12:23:25 +0300

    Add root hints to ansible deploy driver
    This patch adds support for all documented Ironic root hints
    to ansible deploy driver.
    
    Change-Id: I6fd468cfa9a7792fa0e55aa58c337396e13d76ee
    Related-bug: #1526308

--- ironic_staging_drivers/ansible/deploy.py
+++ ironic_staging_drivers/ansible/deploy.py
@@ -351,6 +351,11 @@ def _parse_partitioning_info(node):
     return i_info
 
 
+def _create_root_hints_dict(root_hints):
+    """Convert string with hints to dict. """
+    return dict([key_value.split('=') for key_value in root_hints.split(',')])
+
+
 def _prepare_variables(task):
     node = task.node
     i_info = node.instance_info
@@ -382,6 +387,11 @@ def _prepare_variables(task):
             cfgdrv_type = 'file'
         variables['configdrive'] = {'type': cfgdrv_type,
                                     'location': cfgdrv_location}
+
+    root_device = deploy_utils.parse_root_device_hints(node)
+    if root_device:
+        variables['root_device_hints'] = _create_root_hints_dict(root_device)
+
     return variables
 
 
---
+++ ironic_staging_drivers/ansible/playbooks/library/facts_wwn.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+try:
+    import pyudev
+    HAS_PYUDEV = True
+except ImportError:
+    HAS_PYUDEV = False
+
+
+COLLECT_INFO = (('wwn', 'WWN'), ('serial', 'SERIAL_SHORT'),
+                ('wwn_with_extension', 'WWN_WITH_EXTENSION'),
+                ('wwn_vendor_extension', 'WWN_VENDOR_EXTENSION'))
+
+
+def get_devices_wwn(devices):
+
+    if not HAS_PYUDEV:
+        # can not collect wwns
+        return
+
+    dev_dict = {}
+    context = pyudev.Context()
+    for device in devices:
+        name = '/dev/' + device
+        try:
+            udev = pyudev.Device.from_device_file(context, name)
+        except (ValueError, EnvironmentError, pyudev.DeviceNotFoundError):
+            continue
+
+        dev_dict[device] = {}
+        for key, udev_key in COLLECT_INFO:
+            dev_dict[device][key] = udev.get('ID_%s' % udev_key)
+
+    return {"ansible_facts": {"devices_wwn": dev_dict}}
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            devices=dict(required=True, type='list'),
+        ),
+        supports_check_mode=True,
+    )
+
+    devices = module.params['devices']
+    data = get_devices_wwn(devices)
+    module.exit_json(**data)
+
+
+from ansible.module_utils.basic import *  # noqa
+if __name__ == '__main__':
+    main()
