156fca8b799056d22c70256c5952a98971485c3d
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2016-10-03 21:37:52 +0300

    Re-use tinyipa image for ansbile-deploy
    It is possible to rebuild the pre-built tinyipa image available
    at tarballs.openstack.org to make it usable with ansible-deploy driver.
    
    The rebuild is rather fast, and only downloads 2 packages from TC mirrors
    (SSH server and dependencies).
    
    Change-Id: Ie39ce67dc93e7d53bf75937c7defacafad5fbfcf
    Related-Bug: #1526308

---
+++ imagebuild/tinyipa-ansible/install-deps.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PACKAGES="wget unzip sudo"
+
+echo "Installing dependencies:"
+
+if [ -x "/usr/bin/apt-get" ]; then
+    sudo -E apt-get update
+    sudo -E apt-get install -y $PACKAGES
+elif [ -x "/usr/bin/dnf" ]; then
+    sudo -E dnf install -y $PACKAGES
+elif [ -x "/usr/bin/yum" ]; then
+    sudo -E yum install -y $PACKAGES
+else
+    echo "No supported package manager installed on system. Supported: apt, yum, dnf"
+    exit 1
+fi
