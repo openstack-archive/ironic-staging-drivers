aa3b820e9603e833466db01179daef5aab20e2d0
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2017-09-19 16:37:01 +0000

    [ansible] allow to override Ansible version
    This patch adds new devstack plugin option
    IRONIC_STAGING_ANSIBLE_VERSION_SPEC
    (defaults to empty).
    
    When set, it must be a pip-compatible version spec for Ansible
    to be installed instead of what's specified in
    `ironic_staging_drivers/ansible/driver-requirements.txt` file.
    
    This will allow to implement both bitrot-like jobs (to test previous
    stable Ansible versions) and new release jobs (if we ever cap Ansible
    version in driver-requirements.txt).
    
    Change-Id: I1b7101cf4489827b6fb370b88ccd53aff20a8e8e

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -14,6 +14,7 @@ fi
 if [[ -n "$IRONIC_STAGING_DRIVERS_FILTERS" ]]; then
     IRONIC_STAGING_LIST_EP_CMD+=" -f $IRONIC_STAGING_DRIVERS_FILTERS"
 fi
+IRONIC_STAGING_ANSIBLE_VERSION_SPEC=${IRONIC_STAGING_ANSIBLE_VERSION_SPEC:-}
 
 function setup_ironic_enabled_interfaces_for {
 
@@ -106,6 +107,13 @@ function install_drivers_dependencies {
             fi
         fi
     done
+    if [ -n ${IRONIC_STAGING_ANSIBLE_VERSION_SPEC} ]; then
+        # NOTE(pas-ha) re-install another Ansible version
+        # useful for bitrot-like jobs to test with previous releases
+        # or (if we ever cap Ansible version) with new releases
+        echo_summary "Overriding Ansible version to ${IRONIC_STAGING_ANSIBLE_VERSION_SPEC}"
+        pip_install "ansible${IRONIC_STAGING_ANSIBLE_VERSION_SPEC}"
+    fi
 }
 
 function set_ironic_testing_driver {
