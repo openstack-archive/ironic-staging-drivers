6f235e7b36ec8f7363d95808f3c2dfb57aed5091
Derek Higgins <derekh@redhat.com>
2018-04-09 11:21:15 +0100

    Convert ovirt url to a str
    Older versions if pyculr.Curl.setopt don't accept unicode
    string values.
    
    Change-Id: I4fca91228435a1a652b98db69aa2a92ffbaa47e7
    Closes-Bug: #1761724

--- ironic_staging_drivers/tests/unit/ovirt/test_ovirt.py
+++ ironic_staging_drivers/tests/unit/ovirt/test_ovirt.py
@@ -58,6 +58,20 @@ class oVirtDriverTestCase(db_base.DbTestCase):
         self.assertEqual('changeme', params['ovirt_password'])
         self.assertEqual('jimi', params['ovirt_vm_name'])
 
+    @mock.patch.object(ovirt_power, "sdk", create=True)
+    def test_getvm_unicode(self, sdk):
+        self.node['driver_info']['ovirt_address'] = u'127.0.0.1'
+        driver_info = ovirt_power._parse_driver_info(self.node)
+
+        ovirt_power._getvm(driver_info)
+        ovirt_power.sdk.Connection.assert_called_with(
+            ca_file=None, insecure='False', password='changeme',
+            url='https://127.0.0.1/ovirt-engine/api',
+            username='jhendrix@internal'
+        )
+        url = ovirt_power.sdk.Connection.mock_calls[0][-1]['url']
+        self.assertEqual(type(''), type(url))
+
     def test_get_properties(self):
         expected = list(ovirt_power.PROPERTIES.keys())
         with task_manager.acquire(
