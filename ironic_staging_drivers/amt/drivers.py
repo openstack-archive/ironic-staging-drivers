9a4c70de6e994e807ac6fa921601477d522470eb
Dmitry Tantsur <divius.inside@gmail.com>
2018-05-11 19:14:31 +0200

    [WIP] Remove classic drivers
    Change-Id: I27c61a01184b1d5435e05efa3c65eae90c9d3b34

--- ironic_staging_drivers/amt/drivers.py
+++ ironic_staging_drivers/amt/drivers.py
@@ -13,85 +13,12 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from ironic.common import exception as ironic_exception
-from ironic.drivers import base
 from ironic.drivers import generic
 from ironic.drivers.modules import agent
-from ironic.drivers.modules import fake
-from ironic.drivers.modules import pxe
-from oslo_log import log as logging
-from oslo_utils import importutils
-
 
 from ironic_staging_drivers.amt import deploy as amt_deploy
 from ironic_staging_drivers.amt import management as amt_management
 from ironic_staging_drivers.amt import power as amt_power
-from ironic_staging_drivers.common.i18n import _
-
-LOG = logging.getLogger(__name__)
-
-
-# NOTE(lintan) There is a strange behavior for tox if put below classes
-# in __init__.py. It will reload pywsman and set it to None. So place
-# them here at moment.
-class FakeAMTFakeDriver(base.BaseDriver):
-    """Fake AMT driver."""
-
-    def __init__(self):
-        self.boot = fake.FakeBoot()
-        self.power = amt_power.AMTPower()
-        self.deploy = fake.FakeDeploy()
-        self.management = amt_management.AMTManagement()
-
-
-class PXEAndAMTISCSIDriver(base.BaseDriver):
-    """PXE + AMT + iSCSI driver.
-
-    This driver implements the `core` functionality, combining
-    :class:`ironic.drivers.modules.pxe.PXEBoot` for boot and
-    :class:`ironic_staging_drivers.amt.AMTPower` for power on/off and
-    :class:`ironic.drivers.modules.iscsi_deploy.ISCSIDeploy` for image
-    deployment. Implementations are in those respective classes; this
-    class is merely the glue between them.
-    """
-
-    def __init__(self):
-        LOG.warning("This driver is deprecated and will be removed "
-                    "in the Rocky release. "
-                    "Use 'staging-amt' hardware type instead.")
-        if not importutils.try_import('pywsman'):
-            raise ironic_exception.DriverLoadError(
-                driver=self.__class__.__name__,
-                reason=_("Unable to import pywsman library"))
-        self.power = amt_power.AMTPower()
-        self.boot = pxe.PXEBoot()
-        self.deploy = amt_deploy.AMTISCSIDeploy()
-        self.management = amt_management.AMTManagement()
-
-
-class PXEAndAMTAgentDriver(base.BaseDriver):
-    """PXE + AMT + Agent driver.
-
-    This driver implements the `core` functionality, combining
-    :class:`ironic.drivers.modules.pxe.PXEBoot` for boot and
-    :class:`ironic_staging_drivers.amt.AMTPower` for power on/off and
-    :class:`ironic.drivers.modules.agent_deploy.AgentDeploy` for image
-    deployment. Implementations are in those respective classes; this
-    class is merely the glue between them.
-    """
-
-    def __init__(self):
-        LOG.warning("This driver is deprecated and will be removed "
-                    "in the Rocky release. "
-                    "Use 'staging-amt' hardware type instead.")
-        if not importutils.try_import('pywsman'):
-            raise ironic_exception.DriverLoadError(
-                driver=self.__class__.__name__,
-                reason=_("Unable to import pywsman library"))
-        self.power = amt_power.AMTPower()
-        self.boot = pxe.PXEBoot()
-        self.deploy = agent.AgentDeploy()
-        self.management = amt_management.AMTManagement()
 
 
 class AMTHardware(generic.GenericHardware):
